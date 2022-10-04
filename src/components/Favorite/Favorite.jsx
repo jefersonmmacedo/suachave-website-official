d4bfe0a1c7eb136c232afa4c0660e5d3d96d112a
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-10-04 20:21:07 -0300

    Favoritos

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,24 +1,268 @@
 ﻿import "./favorite.css";
-import { IoHeart } from "react-icons/io5";
-import Profile from "../../assets/images/profile.png";
+import {FaBed, FaShower, FaCar} from 'react-icons/fa';
+import {IoCrop, IoHeart, IoHeartOutline, IoMove} from 'react-icons/io5';
+import ImageHouse from "../../assets/images/house.jpg";
+import ImageHouse1 from "../../assets/images/house1.jpg";
+import ImageHouse2 from "../../assets/images/house2.jpg";
 
 export function Favorite() {
     return (
-        <div className="Favorite">
-            <div className="infos">
-                <div className="image">
-                    <img src={Profile} alt="" />
-                </div>
-                <div className="text">
-                <h4>Nome Completo de Usuário</h4>
-                <div className="professionals">
-                        <p>Profissão</p>
-                        <p>Profissão</p>
-                        <p>Profissão</p>
+        <div className="ListProperty">
+            <div className="listProperty">
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
                     </div>
-                </div>
-            </div>
-            <IoHeart />
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
+                    </div>
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
+                    </div>
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
+                    </div>
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
+                    </div>
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+            
+            <div className="propertyUnicFavorite">
+                    <div className="image">
+                    <img src={ImageHouse} alt="" />
+                    </div>
+                    <div className="text">
+                    <h2>Lindo apartamento luxuoso</h2>
+                    <h4>Centro - Rio Bonito - Rio de Janeiro</h4>
+                    <div className="icons">
+                        <div className="iconUnic">
+                            <p>Quartos</p>
+                            <div className="simbol">
+                                <FaBed />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Banheiro</p>
+                            <div className="simbol">
+                                <FaShower />
+                                <p>3</p>
+                            </div>
+                        </div>
+                        <div className="iconUnic">
+                            <p>Garagem</p>
+                            <div className="simbol">
+                                <FaCar />
+                                <p>3</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div className="pricing">
+                        <h5>Aluguel</h5>
+                        <h2>R$ 2.000,00 / Mês</h2>
+                    </div>
+                    </div>
+                    <div className="text2">
+                        <div className="heart2">
+                        <IoHeart />
+                        </div>
+                    </div>
+                </div> 
+
+                </div>        
         </div>
     )
 }
