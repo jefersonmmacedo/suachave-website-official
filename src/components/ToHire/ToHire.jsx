10c0e8ecc2379fdd774a0d71b7618d211b445b31
Jeferson Macedo <jefersonmacedowgf@gmail.com>
2022-10-29 21:57:04 -0300

    Página em breve

--- src/components/ToHire/ToHire.jsx
+++ src/components/ToHire/ToHire.jsx
@@ -10,7 +10,7 @@ export function ToHire() {
                 </div>
             </div>
             <div className="textToHire">
-                <h3>Veja o que podemos fazer pela sua imobiliária:</h3>
+                <h2>Veja o que podemos fazer pela sua <span>imobiliária:</span> </h2>
                 <div className="itensToHire">
                     <div className="ToHireUnic">
                     <IoHomeOutline />
