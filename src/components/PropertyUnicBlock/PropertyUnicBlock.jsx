c8685908452e0fdb5c4376867f0814a2a84bcf7a
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2022-12-27 18:12:52 -0300

    alterabdo buttons boxProperty

--- src/components/PropertyUnicBlock/PropertyUnicBlock.jsx
+++ src/components/PropertyUnicBlock/PropertyUnicBlock.jsx
@@ -40,17 +40,21 @@ export function PropertyUnicBlock({id}) {
                     </div>
                     : ""
                     } 
-
+                    <div className="infosPropertyButtons">
                     {property?.financing === "" ? "" :
                         <div className="status">
                             <p>Aceita financiamento</p>
                         </div>
                     }
+                    <div className="infosButtons">
+                  
+                    <div className="share">
+                    <NewShareBox idProperty={property?.id} title={property?.title}/>
+                    </div>
                     <div className="heart2">
                     <NewFavorite idProperty={property?.id} idCompany={property?.idCompany} page={"not"}/>
                     </div>
-                    <div className="share">
-                    <NewShareBox idProperty={property?.id} title={property?.title}/>
+                    </div>
                     </div>
                     <div className="text">
                     <a href={`/imovel/${property?.id}`}>
